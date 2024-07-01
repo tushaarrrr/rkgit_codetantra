e1f28b2dd7efa8b06f1ecf94352e0a44f0ce32c1
Tushar <guptatusharr@gmail.com>
2024-07-01 13:16:58 +0530

    Fourth Solution

---
+++ OOPS Programming Lab/Reverse order of Command-line Arguments.java
@@ -0,0 +1,12 @@
+package q21522;
+
+// write your code here 
+public class ReveresedArguments{
+	public static void main(String args[]){
+		for(int i=3; i>=0; i--){
+			System.out.print(args[i]);
+		}
+		System.out.println();
+	}
+}
+
