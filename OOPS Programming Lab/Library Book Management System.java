276ed820f6d59db62de4e98ade6b57a394b4abd0
Tushar <guptatusharr@gmail.com>
2024-07-01 22:17:26 +0530

    Fifth Solution

---
+++ OOPS Programming Lab/Library Book Management System.java
@@ -0,0 +1,40 @@
+package q22939;
+
+import java.util.Scanner;
+
+public class Book {
+  
+	// write your code here..	// write your coe here..
+	String bookTitle;
+	int bookpublicationYear;
+	void setTitle(String title){
+		bookTitle=title;
+	}
+	void setPublicationYear(int publicationYear){
+		bookpublicationYear=publicationYear;
+	}
+	String getTitle(){
+		return bookTitle;
+	}
+	int getPublicationYear(){
+		return bookpublicationYear;
+	}
+	public static void main(String[] args){
+		Book libraryBook =new Book();
+		Scanner scanner= new Scanner(System.in);
+  
+        System.out.print("Title of the book: ");
+        String userEnteredTitle = scanner.nextLine();
+        libraryBook.setTitle(userEnteredTitle);
+
+        System.out.print("Publication year of the book: ");
+        int userEnteredYear = scanner.nextInt();
+        libraryBook.setPublicationYear(userEnteredYear);
+
+        // Display details
+        System.out.println("Details of the Library Book:");
+        System.out.println("Title: " + libraryBook.getTitle());
+        System.out.println("Publication Year: " + libraryBook.getPublicationYear());
+		scanner.close();
+    }
+}
