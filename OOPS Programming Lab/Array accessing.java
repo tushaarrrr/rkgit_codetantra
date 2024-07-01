0b47d9cc28db2da1166a2c45beb4477e41667115
Tushar <guptatusharr@gmail.com>
2024-07-01 22:24:28 +0530

    Nine Solution

---
+++ OOPS Programming Lab/Array accessing.java
@@ -0,0 +1,28 @@
+package q28186;
+import java.util.Scanner;
+
+public class ArrayIndexHandler {
+    public static void main(String[] args) {
+
+	// write the code...
+Scanner scanner = new Scanner(System.in);
+		int size= scanner.nextInt();
+		int[] arr =new int[size];
+		for(int i=0;i<size;i++){
+			arr[i]=scanner.nextInt();
+		}
+		int index=scanner.nextInt();
+
+
+     try {
+             int element=arr[index];
+
+            
+            System.out.println("Element at index " + index + " is: " +element);
+            
+        } catch ( ArrayIndexOutOfBoundsException e    ) {
+        	
+            System.out.println("Error: Index out of bounds");
+        }
+    }
+}
